1181296fbeda75f70e057dd65cc0e5178a09ecf1
James Langford <derivative7117@live.com>
2017-08-03 15:39:00 -0600

    added passport oauth

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const port = 3072,
     express = require('express'),
+    passport = require('passport'),
+    googleStrategy = require('passport-google-oauth20').Strategy,
     app = express();
 
+passport.use(new googleStrategy());
 
 
 app.get('/', (req, res) => {
