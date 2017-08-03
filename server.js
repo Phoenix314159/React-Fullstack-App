c88298da9db02aa8513f1aa93519944a219530b2
James Langford <derivative7117@live.com>
2017-08-03 16:08:49 -0600

    added config file

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const port = 3072,
     express = require('express'),
     passport = require('passport'),
     googleStrategy = require('passport-google-oauth20').Strategy,
+    config = require('./config'),
     app = express();
 
-passport.use(new googleStrategy());
+passport.use(new googleStrategy({
+    clientID: config.googleClientID,
+    clientSecret: config.googleClientSecret
+}));
 
 
 app.get('/', (req, res) => {
