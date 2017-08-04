47b564f338dbc4a88b33f4e4a8b1c45ed949dc2f
James Langford <derivative7117@live.com>
2017-08-03 18:42:14 -0600

    modified server.js

--- server.js
+++ server.js
@@ -7,14 +7,24 @@ const port = 3072,
 
 passport.use(new googleStrategy({
     clientID: config.googleClientID,
-    clientSecret: config.googleClientSecret
+    clientSecret: config.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log('access token', accessToken);
+    console.log('refresh token', refreshToken);
+    console.log('profile', profile);
 }));
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 
 app.get('/', (req, res) => {
     res.status(200).send({ bye: 'buddy'})
 });
-
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
