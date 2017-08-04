1684d3a983b23c579e6b39adaa2e2f1d12d0fbcd
James Langford <derivative7117@live.com>
2017-08-03 19:09:19 -0600

    refactored code

--- server.js
+++ server.js
@@ -1,30 +1,8 @@
 const port = 3072,
     express = require('express'),
-    passport = require('passport'),
-    googleStrategy = require('passport-google-oauth20').Strategy,
-    config = require('./config'),
     app = express();
-
-passport.use(new googleStrategy({
-    clientID: config.googleClientID,
-    clientSecret: config.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    console.log('access token', accessToken);
-    console.log('refresh token', refreshToken);
-    console.log('profile', profile);
-}));
-
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}))
-
-app.get('/auth/google/callback', passport.authenticate('google'));
-
-
-app.get('/', (req, res) => {
-    res.status(200).send({ bye: 'buddy'})
-});
+require('./services/passport');
+require('./routes/authRoutes')(app);
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
---
+++ services/passport.js
@@ -0,0 +1,13 @@
+const  passport = require('passport'),
+    googleStrategy = require('passport-google-oauth20').Strategy,
+    config = require('../config');
+
+passport.use(new googleStrategy({
+    clientID: config.googleClientID,
+    clientSecret: config.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log('access token', accessToken);
+    console.log('refresh token', refreshToken);
+    console.log('profile', profile);
+}));
