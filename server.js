7bcf2e1a8402a93036b473e5deb291268e3b751d
James Langford <derivative7117@live.com>
2017-08-26 11:57:02 -0600

    heroku push

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 const config = require('./config'),
     express = require('express'),
-    cookieSession = require('cookie-session'),
-    passport = require('passport'),
     mongoose = require('mongoose'),
     app = express();
 
 mongoose.connect(config.db);
 
-app.use(
-    cookieSession({
-        maxAge: config.cookieAge,
-        keys: [config.cookieKey]
-    })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
+require('./services/middleware')(app);
 require('./models/user'); // has to be required first so passport can access users model
 require('./services/passport');
 require('./routes/auth')(app);
